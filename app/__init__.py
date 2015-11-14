09f195b0795584f385b23824ef97599ddfdabc66
Encrylize <encrylize@gmail.com>
2015-11-14 17:11:06 +0100

    Add LoginManager initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import config
 
 db = SQLAlchemy()
+lm = LoginManager()
 
 
 def create_app(config_name):
@@ -11,6 +13,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     db.init_app(app)
+    lm.init_app(app)
 
     from app.views import views
     app.register_blueprint(views)
