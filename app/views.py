6271876f39bdb6e394bca23512382dd78444b3b9
Encrylize <encrylize@gmail.com>
2015-11-15 19:21:48 +0100

    Add placeholder index view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from flask import Blueprint
+from flask_login import login_required
 
 views = Blueprint("views", __name__)
+
+@views.route("/")
+@views.route("/index")
+@login_required
+def index():
+    return "Logged in"
