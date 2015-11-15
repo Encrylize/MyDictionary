963592ea732c78b865d3deb66b0056df8064e1c1
Encrylize <encrylize@gmail.com>
2015-11-15 19:17:40 +0100

    Add Flask-OAuthlib initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_oauthlib.client import OAuth
 
 from config import config
 
 db = SQLAlchemy()
 lm = LoginManager()
+oauth = OAuth()
 
 
 def create_app(config_name):
@@ -14,6 +16,7 @@ def create_app(config_name):
 
     db.init_app(app)
     lm.init_app(app)
+    oauth.init_app(app)
 
     from app.views import views
     app.register_blueprint(views)
