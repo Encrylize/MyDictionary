851b2b27ee77df42d95fbc2f1e9638b7dbd2a11b
Encrylize <encrylize@gmail.com>
2015-11-15 18:05:34 +0100

    Change whitespace distribution

--- manage.py
+++ manage.py
@@ -8,14 +8,13 @@ from app.models import User
 app = create_app(os.getenv("MYDICTIONARY_CONFIG") or "default")
 
 migrate = Migrate(app, db)
-
 manager = Manager(app)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User)
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
-
 
 if __name__ == "__main__":
     manager.run()
