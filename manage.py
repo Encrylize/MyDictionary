fc7c52a489a6113b7b26607c42c6f5b38b2feb85
Encrylize <encrylize@gmail.com>
2015-11-16 19:52:20 +0100

    Add Dictionary to shell context

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from config import basedir
 from app import create_app, db
-from app.models import User
+from app.models import User, Dictionary
 
 app = create_app(os.getenv("MYDICTIONARY_CONFIG") or "default")
 
@@ -13,7 +13,7 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Dictionary=Dictionary)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
