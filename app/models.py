19ae8d6e26306567e536201db999459d8a147c97
Encrylize <encrylize@gmail.com>
2015-11-17 17:24:19 +0100

    Remove email column from User

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     social_id = db.Column(db.String(), nullable=False, unique=True)
     name = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String())
     dictionary = db.relationship("Dictionary", backref="creator", uselist=False, cascade="all, delete-orphan")
 
     def __init__(self, **kwargs):
@@ -16,7 +15,7 @@ class User(UserMixin, db.Model):
         self.dictionary = Dictionary(creator=self)
 
     def __repr__(self):
-        return "<User ID: %r, social_id: %r, name: %r, email: %r>" % (self.id, self.social_id, self.name, self.email)
+        return "<User ID: %r, social_id: %r, name: %r>" % (self.id, self.social_id, self.name)
 
 
 class Dictionary(db.Model):
