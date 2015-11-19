bb951b655ca49b341fec3f6cb813f2b07c118696
Encrylize <encrylize@gmail.com>
2015-11-19 18:03:26 +0100

    Add utility function for getting the redirect target

--- app/utils.py
+++ app/utils.py
@@ -15,3 +15,11 @@ def is_safe_url(target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ("http", "https") and ref_url.netloc == test_url.netloc
+
+def get_redirect_target():
+    """ Returns the redirect target. """
+    for target in request.args.get("next"), request.referrer:
+        if not target:
+            continue
+        elif is_safe_url(target):
+            return target
