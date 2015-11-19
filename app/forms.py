7ea0cbe703e2f33648cacf3e4bf7ec3823ea9fe8
Encrylize <encrylize@gmail.com>
2015-11-19 19:22:47 +0100

    Add word class forms

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,10 @@
 from flask import redirect, url_for
 from flask_wtf import Form
-from wtforms import HiddenField
+from wtforms import HiddenField, StringField, BooleanField, SelectField, TextAreaField
+from wtforms.validators import DataRequired
 
 from app.utils import get_redirect_target, is_safe_url
+
 
 class RedirectForm(Form):
     """
@@ -26,3 +28,43 @@ class RedirectForm(Form):
 
         target = get_redirect_target()
         return redirect(target or url_for(endpoint, **values))
+
+
+class WordForm(RedirectForm):
+    pass
+
+
+class NounForm(WordForm):
+    singular = StringField("Singular", [DataRequired()])
+    plural = StringField("Plural", [DataRequired()])
+    gender = SelectField("Gender", choices=[("neuter", "neuter"), ("feminine", "feminine"), ("masculine", "masculine")])
+
+
+class VerbForm(WordForm):
+    infinitive = StringField("Infinitive", [DataRequired()])
+    past_tense = StringField("Past Tense", [DataRequired()])
+    present_perfect_tense = StringField("Present Perfect Tense", [DataRequired()])
+    consonant_change = BooleanField("Consonant Change - 2./3. pers. sgl.")
+
+
+class AdjectiveForm(WordForm):
+    positive = StringField("Positive", [DataRequired()])
+    comparative = StringField("Comparative", [DataRequired()])
+    superlative = StringField("Superlative", [DataRequired()])
+
+
+class AdverbForm(WordForm):
+    adverb = StringField("Adverb", [DataRequired()])
+
+
+class ConjunctionForm(WordForm):
+    conjunction = StringField("Conjunction", [DataRequired()])
+
+
+class PrepositionForm(WordForm):
+    preposition = StringField("Preposition", [DataRequired()])
+    accusative = BooleanField("Accusative")
+    dative = BooleanField("Dative")
+
+WordForm.meaning = TextAreaField("Meaning", [DataRequired()])
+WordForm.examples = TextAreaField("Examples", [DataRequired()])
