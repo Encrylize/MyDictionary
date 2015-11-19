25ec9d2ee99437d0b68f53c9a4f1f5bc72ecf71f
Encrylize <encrylize@gmail.com>
2015-11-19 18:03:05 +0100

    Add utility function for checking URL safety

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlparse, urljoin
+from flask import request
+
 def get_or_create(model, **kwargs):
     """ Returns an instance of model and whether or not it already existed in a tuple. """
     instance = model.query.filter_by(**kwargs).first()
@@ -6,3 +9,9 @@ def get_or_create(model, **kwargs):
     else:
         instance = model(**kwargs)
         return instance, True
+
+def is_safe_url(target):
+    """ Checks if an URL is safe. """
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ("http", "https") and ref_url.netloc == test_url.netloc
