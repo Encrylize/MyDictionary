2bb893673be54286baec3bf11039ccc636ffe6f4
Encrylize <encrylize@gmail.com>
2015-11-20 16:27:35 +0100

    Add test for word class converter

---
+++ tests/test_converters.py
@@ -0,0 +1,25 @@
+import unittest
+from werkzeug.routing import ValidationError
+
+from app import create_app, db
+from app.models import Noun, Verb
+from app.converters import WordClassConverter
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.app_ctx = self.app.app_context()
+        self.app_ctx.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_ctx.pop()
+
+    def test_word_class_converter(self):
+        word_class_converter = WordClassConverter(None)
+        self.assertEquals(word_class_converter.to_python("noun"), Noun)
+        self.assertEquals(word_class_converter.to_python("verb"), Verb)
+        self.assertRaises(ValidationError, word_class_converter.to_python, "invalid_word_class")
