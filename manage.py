e49e7484987e3b508802adbd9e05b2b156eb6bdd
Encrylize <encrylize@gmail.com>
2015-11-20 17:22:52 +0100

    Add Word model to shell context

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from config import basedir
 from app import create_app, db
-from app.models import User, Dictionary
+from app.models import User, Dictionary, Word
 
 app = create_app(os.getenv("MYDICTIONARY_CONFIG") or "default")
 
@@ -14,7 +14,7 @@ manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Dictionary=Dictionary)
+    return dict(app=app, db=db, User=User, Dictionary=Dictionary, Word=Word)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
